426a042239e4465e60fc8a58335606f0963ab1b2
Cadavinci <cadavincinfo@gmail.com>
2025-04-30 14:21:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ def encode_image(image_file):
 
 st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
-st.title("Análisis de Imagen:🤖🏞️")
+st.title("🏞️🤖Análisis de Imagen🤖🏞️")
 ke = st.text_input('Ingresa tu Clave')
 os.environ['OPENAI_API_KEY'] = ke
 
-
+st.image("imagen.jpg")
 # Retrieve the OpenAI API Key from secrets
 api_key = os.environ['OPENAI_API_KEY']
 
